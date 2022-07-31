47a936e6091bbf134ad158662d02e759c467180b
fatmaakkaya853 <fatmaakkaya853@gmail.com>
2022-07-31 20:41:27 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+namespace switch_case
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            //Expression= kontrol edilmek istenen koşul
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine ("Ocak ayındasınız.");
+                    break;
+                
+                case 2:
+                    Console.WriteLine ("Şubat ayındasınız.");
+                    break;
+
+                case 7:
+                    Console.WriteLine ("Temmuz ayındasınız.");
+                    break;
+                default:
+
+                    Console.WriteLine ("Yanlış veri girdiniz.");
+                break;
+            }
+
+
+            
+            switch (month)
+            {
+
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine ("kış mevsimindesiniz.");
+                    break;
+                
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine ("İlkbahar mevsimindesiniz.");
+                    break;
+
+                case 6 :
+                case 7:
+                case 8:
+                    Console.WriteLine ("Yaz mevsimindesiniz.");
+                    break;
+
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine ("Sonbahar mevsimindesiniz.");
+                    break;
+                default:
+                break;
+            }
+
+
+            
+
+        }
+        
+    }
+}
+
